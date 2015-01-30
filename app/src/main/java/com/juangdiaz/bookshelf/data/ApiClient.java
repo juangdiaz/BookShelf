a1c53a77a286dbc7479a880f9b0bc27061a506c7
Juan Diaz <Juan@integrativenutrition.com>
2015-01-30 17:11:43 -0500

    adding all the calls to the apiclient

--- app/src/main/java/com/juangdiaz/bookshelf/data/ApiClient.java
+++ app/src/main/java/com/juangdiaz/bookshelf/data/ApiClient.java
@@ -6,7 +6,12 @@ import java.util.List;
 
 import retrofit.Callback;
 import retrofit.RestAdapter;
+import retrofit.http.DELETE;
+import retrofit.http.Field;
+import retrofit.http.FormUrlEncoded;
 import retrofit.http.GET;
+import retrofit.http.POST;
+import retrofit.http.PUT;
 import retrofit.http.Path;
 
 /**
@@ -34,6 +39,33 @@ public class ApiClient {
 
         @GET("/books/{id}/")
         void detailBook(@Path("id") int id,Callback<Book> response);
+
+        @FormUrlEncoded
+        @POST("/books/")
+        Book createBook(
+                @Field("author") String author,
+                @Field("categories") String categories,
+                @Field("title") String title,
+                @Field("publisher") String publisher,
+                @Field("lastCheckedOutBy") String lastCheckedOutBy
+        );
+
+
+        @FormUrlEncoded
+        @PUT("/books/{id}")
+        void checkoutBook(
+                @Path("id") int id,
+                @Field("lastCheckedOutBy") String checkOutBy
+        );
+
+
+        @DELETE("/books/{id}/")
+        void deleteBookById(@Path("id") int id);
+
+
+        @DELETE("/clean/")
+        void deleteAllBooks(Callback<List<Book>> response);
+
 
     }
 
